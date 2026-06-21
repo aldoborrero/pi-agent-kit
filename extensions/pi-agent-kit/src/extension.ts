349bbba0de0267e622ee17d61a698be085f98c83
Aldo Borrero <82811+aldoborrero@users.noreply.github.com>
2026-06-21 19:10:58 +0200

    fix(pi-agent-kit): correct relative import paths in bundle extension

--- extensions/pi-agent-kit/src/extension.ts
+++ extensions/pi-agent-kit/src/extension.ts
@@ -6,30 +6,30 @@
  */
 
 import type { ExtensionAPI } from "@earendil-works/pi-coding-agent";
-import registerAstGrep from "../../extensions/pi-ast-grep/src/extension.ts";
-import registerBitwarden from "../../extensions/pi-bitwarden/src/extension.ts";
-import registerBtw from "../../extensions/pi-btw/src/extension.ts";
-import registerCodex from "../../extensions/pi-codex/src/extension.ts";
-import registerContext from "../../extensions/pi-context/src/extension.ts";
-import registerDiff from "../../extensions/pi-diff/src/extension.ts";
-import registerDirenv from "../../extensions/pi-direnv/src/extension.ts";
-import registerExit from "../../extensions/pi-exit/src/extension.ts";
-import registerFooter from "../../extensions/pi-footer/src/extension.ts";
-import registerGitCheckpoint from "../../extensions/pi-git-checkpoint/src/extension.ts";
-import registerGithubSearch from "../../extensions/pi-github-search/src/extension.ts";
-import registerInlineBash from "../../extensions/pi-inline-bash/src/extension.ts";
-import registerLoop from "../../extensions/pi-loop/src/extension.ts";
-import registerNotify from "../../extensions/pi-notify/src/extension.ts";
-import registerQuestionnaire from "../../extensions/pi-questionnaire/src/extension.ts";
-import registerSandbox from "../../extensions/pi-sandbox/src/extension.ts";
-import registerSubagent from "../../extensions/pi-subagent/src/extension.ts";
-import registerSuggest from "../../extensions/pi-suggest/src/extension.ts";
-import registerTuicr from "../../extensions/pi-tuicr/src/extension.ts";
-import registerUntil from "../../extensions/pi-until/src/extension.ts";
-import registerVoice from "../../extensions/pi-voice/src/extension.ts";
-import registerWalkie from "../../extensions/pi-walkie/src/extension.ts";
-import registerWebTools from "../../extensions/pi-web-tools/src/extension.ts";
-import registerWorkflows from "../../extensions/pi-workflows/src/extension.ts";
+import registerAstGrep from "../../pi-ast-grep/src/extension.ts";
+import registerBitwarden from "../../pi-bitwarden/src/extension.ts";
+import registerBtw from "../../pi-btw/src/extension.ts";
+import registerCodex from "../../pi-codex/src/extension.ts";
+import registerContext from "../../pi-context/src/extension.ts";
+import registerDiff from "../../pi-diff/src/extension.ts";
+import registerDirenv from "../../pi-direnv/src/extension.ts";
+import registerExit from "../../pi-exit/src/extension.ts";
+import registerFooter from "../../pi-footer/src/extension.ts";
+import registerGitCheckpoint from "../../pi-git-checkpoint/src/extension.ts";
+import registerGithubSearch from "../../pi-github-search/src/extension.ts";
+import registerInlineBash from "../../pi-inline-bash/src/extension.ts";
+import registerLoop from "../../pi-loop/src/extension.ts";
+import registerNotify from "../../pi-notify/src/extension.ts";
+import registerQuestionnaire from "../../pi-questionnaire/src/extension.ts";
+import registerSandbox from "../../pi-sandbox/src/extension.ts";
+import registerSubagent from "../../pi-subagent/src/extension.ts";
+import registerSuggest from "../../pi-suggest/src/extension.ts";
+import registerTuicr from "../../pi-tuicr/src/extension.ts";
+import registerUntil from "../../pi-until/src/extension.ts";
+import registerVoice from "../../pi-voice/src/extension.ts";
+import registerWalkie from "../../pi-walkie/src/extension.ts";
+import registerWebTools from "../../pi-web-tools/src/extension.ts";
+import registerWorkflows from "../../pi-workflows/src/extension.ts";
 
 type Factory = (pi: never) => void;
 
